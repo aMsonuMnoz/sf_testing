f99138af0b391e6e7296a6ee35d8b15c32bf80aa
aMsonuMnoz <114356675+aMsonuMnoz@users.noreply.github.com>
2023-08-03 02:11:48 -0500

    lyrics

--- jq.js
+++ jq.js
@@ -1,6 +1,13 @@
 import $ from 'jquery';
 
 
+function hideAll() {
+    $("#Interface2").css("visibility","hidden");
+    $("#Interface2").css("display","none");
+    $('div[id^="Interface"]').hide();
+    $(".ComputerLoading").remove();
+}
+
 
 
 $(function(){
@@ -8,31 +15,35 @@ $(function(){
     $("#Interface2").css("display","none");
 
     $("#LinksInterface").on("click", function() {
-        $("#Interface2").css("visibility","hidden");
-        $("#Interface2").css("display","none");
-        $(".ComputerLoading").remove();
+        hideAll();
         $("#Interface").show();
         
     });
 
     $("#ThreeDimInterface").on("click", function() {
-
+        hideAll();
         $("#Interface2").css("visibility","visible");
         $("#Interface2").css("display","inline-block");
-        $("#Interface").hide();
-        $(".ComputerLoading").remove();
         
     });
 
     $("#PowerButton").on("click", function() {
-        $("#Interface2").css("visibility","hidden");
-        $("#Interface2").css("display","none");
+        hideAll();
+
         $("#Interface").show();
         $('#Interface').prepend($('<img>',{class:'ComputerLoading',src:'images/CRT_off.webp'}));
         setTimeout(() => {
           $(".ComputerLoading").attr("src","images/Scene8_off.png");
         }, "1000");
 
+    });
+
+    $("#Lyrics").on("click", function() {
+
+        hideAll();
+        $("#Interface3").show();
+        $("#Interface3").css("display","inline-block");
+        
     });
 
 
