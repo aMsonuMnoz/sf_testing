c0cc6e8d054c165f6a2f5c1c1c823996849f2ed5
aMsonuMnoz <114356675+aMsonuMnoz@users.noreply.github.com>
2023-08-22 00:46:24 -0500

    new build contents, waterfal fix

--- bg.js
+++ bg.js
@@ -3,13 +3,13 @@ import $ from 'jquery';
 
 
 export var imageURLs = [
-    "url(images/Scene4-B.webp)"
-  , "url(images/Scene2-B.webp)"
-  , "url(images/Scene3-B.webp)"
-  , "url(images/Scene5-B.webp)"
-  , "url(images/Scene8-glitch.webp)"
-  , "url(images/Scene7-Website.webp)"
-  , "url(gifs/waterfal.webp"
+    "url(/Scene4-B.webp)"
+  , "url(/Scene2-B.webp)"
+  , "url(/Scene3-B.webp)"
+  , "url(/Scene5-B.webp)"
+  , "url(/Scene8-glitch.webp)"
+  , "url(/Scene7-Website.webp)"
+  , "url(/waterfal.webp"
 ];
 export function getImageTag() {
     var randomIndex = Math.floor(Math.random() * imageURLs.length);
@@ -17,7 +17,5 @@ export function getImageTag() {
     return img;
 }
 
-$(function(){
-    $("#overlay-links").css("background",getImageTag());
-    $("#overlay-links").css("background-size","cover");
-});
+$("#overlay-links").css("background",getImageTag());
+$("#overlay-links").css("background-size","cover");
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 
   build: {
     minify: true,
-    sourcemap: true,
+    sourcemap: false,
     assetsInlineLimit: 0
   }
 
